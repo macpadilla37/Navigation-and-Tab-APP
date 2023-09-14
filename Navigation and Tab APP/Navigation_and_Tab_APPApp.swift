f0ed7586a1097431aa8e276ce72b0f70b9c479cc
macpadilla37 <89860555+macpadilla37@users.noreply.github.com>
2023-09-14 17:29:29 -0600

    Initial Commit

---
+++ Navigation and Tab APP/Navigation_and_Tab_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_and_Tab_APPApp.swift
+//  Navigation and Tab APP
+//
+//  Created by Mac Padilla on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_and_Tab_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
